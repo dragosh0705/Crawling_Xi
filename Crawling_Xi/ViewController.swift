16654c299d798ab37d6db549c30678bab4592de4
dragosh0705 <pingu0705@hanyang.ac.kr>
2023-07-14 21:26:37 +0900

    Crawling_html_Fail

--- Crawling_Xi/ViewController.swift
+++ Crawling_Xi/ViewController.swift
@@ -6,14 +6,32 @@
 //
 
 import UIKit
+import SwiftSoup
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        crawl()
     }
+}
 
-
+func crawl() {
+    let url = URL(string: "https://new.land.naver.com/complexes/126726?ms=37.489144,127.025957,17&a=APT:PRE&e=RETAIL")
+    guard let myURL = url else {return}
+    
+    do{
+        let html = try String(URLSession: myURL, encoding: .utf8)
+        let doc : Document = try SwiftSoup.parse(html)
+        let headerTitle = try doc.title()
+        print(headerTitle)
+        
+        let firstLinkTitles: Elements = try doc.select(".data").select("dd")
+    } catch Exception.Error(let type, let message) {
+        print("Message :  \(message)")
+    } catch {
+        print("error")
+    }
 }
 
